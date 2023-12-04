d56ead6b23a3ce27c40d90cea9f255792ab25edf
unknown <Breke64@gmail.com>
2023-12-04 14:46:28 -0500

    fixed some minor bugs with buildpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -5,45 +5,43 @@ const DB_NAME = 'jate'
 
 
 const initdb = async () =>
+console.log('init');
   openDB(DB_NAME, 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains(DB_NAME)) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore(DN_NAME, { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore(DB_NAME, { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 export const putDb = async (content) => {
-  
-  //variable for replacing database with uppercase
+
 
   const OPEN_DB = await openDB(DB_NAME, 1);
   const YOUR_TX = OPEN_DB.transaction(DB_NAME, 'readwrite');
   const YOUR_STORE = YOUR_TX.objectStore(DB_NAME);
-  const request = YOUR_STORE.put({ id: 1, value: content});
+  const request = YOUR_STORE.put({ id: 1, value: content });
   const result = await request;
   console.log(('🚀 - data saved to the database', result.value));
 }
 
 
-// TODO: Add logic for a method that gets all the content from the database
 
 
 export const getDb = async () => {
-  console.error('getDb not implemented');
-const OPEN_DB = await openDB(DB_NAME, 1);
-const YOUR_TX = OPEN_DB.transaction(DB_NAME, 'readonly');
-const YOUR_STORE = YOUR_TX.objectStore(DB_NAME);
-const request = YOUR_STORE.get(1);
-const result = await request;
-result
-  ? console.log('🚀 - data retrieved from the database', result.value)
-  : console.log('🚀 - data not found in the database');
+  const OPEN_DB = await openDB(DB_NAME, 1);
+  const YOUR_TX = OPEN_DB.transaction(DB_NAME, 'readonly');
+  const YOUR_STORE = YOUR_TX.objectStore(DB_NAME);
+  const request = YOUR_STORE.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
 
-return result?.value;
+  return result?.value;
 };
 
 
